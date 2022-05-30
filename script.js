3f3ca685ff12a731abd9d1741f10c77367736d61
WHL99 <xjv298347@gmail.com>
2022-05-30 14:34:05 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log(4 == '4')
+console.log(4 === '4')
+
+console.clear()
+
+if (9>7){
+    console.log('this is true')
+}
+
+let password =''
+
+if (!password)
+{
+    console.log('please enter your password')
+}else{
+    console.log('it is set')
+}
+
+
